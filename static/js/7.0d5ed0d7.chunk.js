e04cf151314112d7953748242103a7e07c292407
warriorswap <97707973+warriorswap@users.noreply.github.com>
2022-01-19 16:54:21 -0500

    Update 7.0d5ed0d7.chunk.js

--- static/js/7.0d5ed0d7.chunk.js
+++ static/js/7.0d5ed0d7.chunk.js
@@ -438,11 +438,11 @@
                     var e = Object(v.a)();
                     return Object(V.jsxs)(k.a, {
                         children: [
-                              Object(V.jsxs)(be, {
-                                        children: [Object(V.jsx)("img", { src: "https://poocoin.app/images/promotions/shieldnetwork.gif", alt: "title", width: 417, height: 90 })],
-                                    }),
                             Object(V.jsxs)(be, {
                                 children: [
+                                    Object(V.jsxs)(be, {
+                                        children: [Object(V.jsx)<a href="https://google.com">("img", { src: "https://poocoin.app/images/promotions/shieldnetwork.gif", alt: "title", width: 417, height: 90 })],
+                                    })</a>,
                                     Object(V.jsx)(g.t, { as: "h1", size: "xl", mb: "24px", color: "secondary", children: e(576, "Warrior Swap") }),
                                     Object(V.jsx)(g.L, { children: e(578, "The #1 Decentralized Exchange for Warriors.") }),
                                 ],
