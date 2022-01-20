cecc3b2d35d4b5bba3eca1db3628c4cfcc856406
warriorswap <97707973+warriorswap@users.noreply.github.com>
2022-01-19 20:50:10 -0500

    Update 7.0d5ed0d7.chunk.js

--- static/js/7.0d5ed0d7.chunk.js
+++ static/js/7.0d5ed0d7.chunk.js
@@ -439,7 +439,7 @@
                     return Object(V.jsxs)(k.a, {
                         children: [
                             Object(V.jsxs)(be, {
-                                children: [Object(V.jsx)(g.t, { as: "a", href: "https://babyfjb.com", target: "_blank", rel: "noreferrer noopener", children: [Object(V.jsx)("img", { src: "https://i.imgur.com/5uqa5Zu.gif", alt: "Banner Icon", width: 1092, height: 135 })] })],
+                                children: [Object(V.jsx)(g.t, { as: "a", href: "", target: "_blank", rel: "noreferrer noopener", children: [Object(V.jsx)("img", { src: "https://i.imgur.com/pxXWZ6h.png", alt: "Banner Icon", width: 1092, height: 135 })] })],
                             }),
                                     Object(V.jsxs)(be, {
                                         children: [Object(V.jsx)(g.t, { as: "h1", size: "xl", mb: "24px", color: "secondary", children: e(576, "Warrior Swap") }),
